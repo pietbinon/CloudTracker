aca40bbff6592c619222175ae88bfbcf591bad58
pietbinon <pierrebinon@icloud.com>
2017-04-11 17:22:05 -0400

    Commit Ar 11, 2017

--- FoodTracker/SignupViewController.swift
+++ FoodTracker/SignupViewController.swift
@@ -77,18 +77,4 @@ class SignupViewController: UIViewController, UITextFieldDelegate {
             self.dismiss(animated: true, completion: nil)
         })
     }
-    
-    
-    
-    
-    /*
-     // MARK: - Navigation
-     
-     // In a storyboard-based application, you will often want to do a little preparation before navigation
-     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-     // Get the new view controller using segue.destinationViewController.
-     // Pass the selected object to the new view controller.
-     }
-     */
-    
 }
